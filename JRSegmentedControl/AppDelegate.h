c804d4c536582ac335504c65576b672d3ff16bbb
王潇 <23601145@qq.com>
2015-12-27 10:51:42 +0800

    Initial Commit

---
+++ JRSegmentedControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRSegmentedControl
+//
+//  Created by wxiao on 15/12/27.
+//  Copyright © 2015年 wxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
