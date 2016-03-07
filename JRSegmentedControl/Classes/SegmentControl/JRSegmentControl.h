e0d018aa37afcb336f008d0379098050da997754
王潇 <23601145@qq.com>
2016-03-07 15:50:03 +0800

    实现最基本的SegmentControl

---
+++ JRSegmentedControl/Classes/SegmentControl/JRSegmentControl.h
@@ -0,0 +1,13 @@
+//
+//  JRSegmentControl.h
+//  JRSegmentedControl
+//
+//  Created by 王潇 on 16/3/7.
+//  Copyright © 2016年 wxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JRSegmentControl : UIControl
+
+@end
---
+++ JRSegmentedControl/Classes/SegmentControl/JRSegmentControl.m
@@ -0,0 +1,21 @@
+//
+//  JRSegmentControl.m
+//  JRSegmentedControl
+//
+//  Created by 王潇 on 16/3/7.
+//  Copyright © 2016年 wxiao. All rights reserved.
+//
+
+#import "JRSegmentControl.h"
+
+@implementation JRSegmentControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
